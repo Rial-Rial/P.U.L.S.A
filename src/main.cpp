fd38cc949ba4b3546efc228234e2970417aaf9af
Rial-iMac <peroquemierda98@icloud.com>
2026-05-19 22:51:38 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,142 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+const int pulser1 = 13;
+const int pulser2 = 12;
+const int pulser3 = 11;
+const int pulser4 = 10;
+const int pulser5 = 9;
+const int motorstep = 7;
+const int motordir = 8;
+const int tempsignal = A0;
+const int tempcontrol = 5;
+
+float temperatureValue = 0.0;
+float steinhart;
+int constantforcontrol = 100;
+float sensorValue;
+float tempC;
+
+int pulsespeed = 300;
+
+void setup() {
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(2, 0);
+  lcd.print("WELLCOME TO");
+  lcd.setCursor(3, 1);
+  lcd.print("P.U.L.S.A");
+  delay(1000);
+  lcd.clear();
+
+
+  pinMode(pulser1, INPUT);
+  pinMode(pulser2, INPUT);
+  pinMode(pulser3, INPUT);
+  pinMode(pulser4, INPUT);
+  pinMode(pulser5, INPUT);
+  pinMode(motorstep, OUTPUT);
+  pinMode(motordir, OUTPUT);
+  pinMode(tempsignal, INPUT);
+  pinMode(tempcontrol, OUTPUT);
+
+  digitalWrite(motordir, HIGH);
+
+  Serial.begin(9600);
+
+  Wire.begin();
+  Serial.println("Scanning for I2C devices...");
+  for (byte i = 1; i < 127; i++) {
+    Wire.beginTransmission(i);
+    if (Wire.endTransmission() == 0) {
+      Serial.print("I2C device found at address 0x");
+      Serial.println(i, HEX);
+    }
+  }
+}
+void motor (){
+
+  digitalWrite(motorstep, HIGH );
+  delay(pulsespeed);
+  digitalWrite(motorstep, LOW);
+  delay(pulsespeed);
+
+}
+
+void readtemperature(){
+  int samples = 5;
+  const float SERIESRESISTOR = 100000.0; 
+  const float NOMINAL_RESISTANCE = 100000.0; 
+  const float NOMINAL_TEMPERATURE = 25.0;  
+  const float BCOEFFICIENT = 3950.0;     
+
+  for (int i = 0; i < samples; i++) {
+    sensorValue += analogRead(tempsignal);
+    delay(100);
+  }
+  sensorValue /= samples; 
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
+
+  sensorValue = 1023.0 / sensorValue - 1.0;
+  float resistance = SERIESRESISTOR / sensorValue;
+
+  steinhart = resistance / NOMINAL_RESISTANCE;     // (R/Ro)
+  steinhart = log(steinhart);                      // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                       // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (NOMINAL_TEMPERATURE + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                     // Invert to get Kelvin
+  
+  tempC = steinhart - 273.15;                // Convert Kelvin to Celsius
+
+  Serial.print("Temperature: ");
+  Serial.print(tempC);
+  Serial.println("°C");
+  Serial.print("Steinhart-Hart Temperature: ");
+  Serial.print(steinhart);
+  Serial.println(" K");
+  Serial.println();
+  delay (100);
+
+}
+
+void setTemperature(){
+  analogWrite(tempcontrol, constantforcontrol);
+  Serial.print("constatntforcontrol: ");
+  Serial.println(constantforcontrol);
+}
+
+void bottons(){
+  if (digitalRead(pulser1) == HIGH) {
+    constantforcontrol = constantforcontrol + 1 ;
+  }
+  if (digitalRead(pulser2) == HIGH){
+    constantforcontrol = constantforcontrol - 1 ;
+  }
+  if (digitalRead(pulser3) == HIGH){
+    pulsespeed = pulsespeed + 10  ;
+  }
+  if (digitalRead(pulser4) == HIGH){
+   pulsespeed = pulsespeed - 10 ;
+  }
+}
+
+void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print("Temperature:");
+  lcd.setCursor(12, 0);
+  lcd.print(tempC);
+  lcd.setCursor(0, 1);
+  lcd.print("Speed:");
+  lcd.setCursor(9, 1);
+  lcd.print(pulsespeed);
+
+bottons();
+Serial.println (pulsespeed);
+motor();
+readtemperature();
+setTemperature();
+}
